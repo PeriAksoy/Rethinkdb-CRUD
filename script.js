4567cdc6da92faf8615513abbe0b20a7f7b5fda7
PeriAksoy <nisaperiaksoy27@gmail.com>
2024-07-27 21:50:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+
+const todoContainer = document.getElementById("todos");
+const Todo_text = document.getElementById("input_text");
+
+//*****POST*****//
+
+function Add_todo() {
+
+  const todoValue = Todo_text.value;
+  const new_todo = document.createElement("div");
+  new_todo.classList.add("todo_item") ; 
+
+  //POST İŞLEMİ
+
+  fetch('http://127.0.0.1:3000',{
+
+    method:'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({ todo : todoValue})
+  })
+  .then(response => response.json())
+  .then(data => {
+    const todoId = data.id;
+    new_todo.id = todoId;
+    new_todo.innerHTML = `
+      <div id="text_${todoId}" class="text">${todoValue}</div>
+      <button class="edit" onclick="Edit_todo('${todoId}')">✎</button>
+      <button class="delete" onclick="Delete_todo('${todoId}')">🗑️</button>
+    `;
+    todoContainer.appendChild(new_todo);
+    todoValue = '';
+  })
+  .catch(error => console.error('Error:', error));
+}
+
+//*****DELETE*****/
+
+const Delete_todo = (todo_Id) => {
+
+fetch(`http://127.0.0.1:3000/${todo_Id}`,{
+
+    method: "DELETE"
+
+  })
+  .then((res) => {
+      const todoItem = document.getElementById(todo_Id);
+      todoItem.remove()
+  })
+  .catch(error => {
+    console.error('Error:',error)
+  });
+}
+
+//*****PUT*****//
+
+const Edit_todo = (todo_Id) => {
+
+  const todo_text_container = document.getElementById(`text_${todo_Id}`);
+  const edit_container = document.createElement("div");
+  edit_container.classList.add("edit_container");
+    
+  const input = document.createElement("input");
+  input.type= "text";
+  
+  const save_button = document.createElement("button");
+  save_button.textContent = "Save";
+  
+  edit_container.appendChild(input);
+  edit_container.appendChild(save_button);
+
+  //text div yerine edit_container gelmesi
+  const todo_item = todo_text_container.parentElement;
+  todo_item.insertBefore(edit_container,todo_text_container);
+  todo_text_container.style.display = "none";
+
+  //todo_item deki buttonların kaldırılması
+  const edit_button = todo_item.querySelector(".edit");
+  const delete_button = todo_item.querySelector(".delete");
+  edit_button.style.display = "none";
+  delete_button.style.display = "none";
+
+  //PUT İŞLEMİ 
+
+  save_button.onclick = () =>{
+  const update_todo = input.value;
+
+  fetch(`http://127.0.0.1:3000/${todo_Id}`, {
+
+    method: "PUT",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ todo: update_todo })
+  })
+  .then(response => response.json())
+  .then(() => {
+    todo_text_container.textContent = update_todo; 
+    todo_text_container.style.display = ""; 
+    edit_button.style.display = "";
+    delete_button.style.display= "";
+    edit_container.remove();
+  })
+  .catch(error => console.error("Error:", error));
+  };
+}
+
---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const r = require('rethinkdbdash')({
+    servers: [
+        {
+            host: 'localhost', 
+            port: 28015        
+        }
+    ]
+});
+
+var app = express();
+
+app.use(bodyParser.json())
+app.use(cors());
+
+//POST
+app.post("/",async (req,res) =>{
+    const todoItem=req.body;
+    await r.db("Todo_list").table("Todos").insert(todoItem).run();
+    res.send(todoItem);
+});
+
+//DELETE
+app.delete("/:id",async (req,res) =>{
+    const todoId = req.params.id;
+    await r.db("Todo_list").table("Todos").get(todoId).delete().run();
+    res.send({
+        message: "başarılı"
+    });
+});
+
+//GET
+
+app.get("/", async (req,res) =>{
+    const todo = await r.db("Todo_list").table("Todos");
+    res.send(todo);
+});
+
+//PUT
+
+app.put ("/:id", async(req,res) =>{
+    const todoId = req.params.id;
+    const newTodo = req.body.todo;
+    await r.db("Todo_list").table("Todos").get(todoId).update({ todo:newTodo}).run();
+    res.send({ id:todoId,todo:newTodo});
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
+
+
+
